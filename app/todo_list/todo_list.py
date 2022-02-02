2226a4fb47ac7c4b9ea567f7f3801eea1205bb70
Jeremiah Djianto <jeremiah.djianto@mail.utoronto.ca>
2022-02-02 00:02:24 -0500

    Implemented User GET and POST

--- app/todo_list/todo_list.py
+++ app/todo_list/todo_list.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
 import ast
+import os
+import sys
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,6 +13,34 @@ class Users(Resource):
     
     Users should have "Todo-Lists" which lists their tasks.
     """
+    def get(self):
+        """Returns all data stored for users if request is successful."""
+        data = pd.read_csv(os.path.join(sys.path[0], "data/users.csv"))
+        data = data.to_dict()
+
+        return {'data': data}, 200 # Return data and "200 OK" code
+
+    def post(self):
+        """Returns all updated user data if request is successful."""
+        parser = reqparse.RequestParser()
+
+        parser.add_argument('userId', required=True)
+        parser.add_argument('name', required=True)
+
+        args = parser.parse_args()
+
+        new_data = pd.DataFrame({
+            'userId': [args['userId']],
+            'name': [args['name']]
+        })
+
+        data = pd.read_csv(os.path.join(sys.path[0], "data/users.csv"))
+        data = data.append(new_data)
+
+        data.to_csv(os.path.join(sys.path[0], "data/users.csv"), index=False)
+        data = data.to_dict()
+
+        return {'data': data}, 200
     pass
 
 class TodoList(Resource):
@@ -20,8 +50,8 @@ class TodoList(Resource):
     """
     pass
 
-api.add_resource(Users, '/users')
-api.add_resource(TodoList, '/locations')
+api.add_resource(Users, "/users")
+api.add_resource(TodoList, "/locations")
 
 if __name__ == "__main__":
     app.run()
