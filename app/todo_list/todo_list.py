471ccfc1b3e701e3662a7625b6208ede94fc2e01
Jeremiah Djianto <jeremiah.djianto@mail.utoronto.ca>
2022-02-03 00:37:56 -0500

    Updated todo_list.py
    Added delete method for User

--- app/todo_list/todo_list.py
+++ app/todo_list/todo_list.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+from numpy import require
 import pandas as pd
 import ast
 import os
@@ -8,7 +9,7 @@ import sys
 app = Flask(__name__)
 api = Api(app)
 
-class Users(Resource):
+class User(Resource):
     """A class to represent a user.
     
     Users should have "Todo-Lists" which lists their tasks.
@@ -18,31 +19,52 @@ class Users(Resource):
         data = pd.read_csv(os.path.join(sys.path[0], "data/users.csv"))
         data = data.to_dict()
 
-        return {'data': data}, 200 # Return data and "200 OK" code
+        return {"data": data}, 200 # Return data and "200 OK" code
 
     def post(self):
         """Returns all updated user data if request is successful."""
         parser = reqparse.RequestParser()
 
-        parser.add_argument('userId', required=True)
-        parser.add_argument('name', required=True)
+        parser.add_argument("userId", required=True)
+        parser.add_argument("name", required=True)
 
         args = parser.parse_args()
 
         new_data = pd.DataFrame({
-            'userId': [args['userId']],
-            'name': [args['name']]
+            "userId": [args["userId"]],
+            "name": [args["name"]]
         })
 
         data = pd.read_csv(os.path.join(sys.path[0], "data/users.csv"))
-        data = data.append(new_data)
 
+        if args["userId"] in list(data["userId"]):
+            return {"message": f"user '{args['userId']}' already exists."}, 401
+
+        data = pd.concat([data, new_data], ignore_index=True)
         data.to_csv(os.path.join(sys.path[0], "data/users.csv"), index=False)
         data = data.to_dict()
 
-        return {'data': data}, 200
-    pass
+        return {"data": data}, 200
+    
+    def delete(self):
+        """Removes specified user from user data if request is successful."""
+        parser = reqparse.RequestParser()
 
+        parser.add_argument("userId", required=True)
+        args = parser.parse_args()
+
+        data = pd.read_csv(os.path.join(sys.path[0], "data/users.csv"))
+
+        if args["userId"] not in list(data["userId"]):
+            return {"message": f"user '{args['userId']}' does not exist."}, 404
+
+        data = data[data.userId != args["userId"]]
+        data.to_csv(os.path.join(sys.path[0], "data/users.csv"), index=False)
+        data = data.to_dict()
+
+        return {"data": data}, 200
+
+        
 class TodoList(Resource):
     """A class to represent a "Todo-List".
     
@@ -50,7 +72,7 @@ class TodoList(Resource):
     """
     pass
 
-api.add_resource(Users, "/users")
+api.add_resource(User, "/users")
 api.add_resource(TodoList, "/locations")
 
 if __name__ == "__main__":
